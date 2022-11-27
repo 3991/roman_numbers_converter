a770c74f482ec09bfa5df4065804d013ecfe0956
aristide <aristide.cardon@gmail.com>
2022-11-27 10:54:02 +0100

    added: to check the command line args are okay

---
+++ src/controls.py
@@ -0,0 +1,4 @@
+import argparse
+
+def command_line_arguments():
+    return 12
---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+from logic import get_arabic
+from controls import command_line_arguments
+
+
+if __name__ == '__main__':
+    nb = command_line_arguments()
